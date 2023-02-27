7c995ba74591a69b6eb8f0f4303a1933bd7b894d
amanmobariz <amanodin.mobariz@tekschoo.us>
2023-02-27 11:55:27 +0000

    thi is third commit for today

--- src/test/git/GitTest.java
+++ src/test/git/GitTest.java
@@ -1,5 +1,13 @@
 package test.git;
 
 public class GitTest {
+	// initialise an empty local git repo - only once - git init (at the Terminal)
+		// to check status of your repo - git status (you have to put - cd and location address)
 
+	public void test() {
+		System.out.println("this is just a test");
+		
+		
+		
+			}
 }
